ffe43854181ee64782404f0e52674e7c7e81f7ad
Ashish Sunkara <asunkara3@wisc.edu>
2025-04-22 22:27:32 -0500

    modified sh script

--- hadamard/run_hadamard.sh
+++ hadamard/run_hadamard.sh
@@ -13,7 +13,7 @@ module load nvidia/cuda/11.8.0
 nvcc main.cu matrix_ops.cu utils.cpp -o hadamard_exec -std=c++17 -O3 -Xcompiler -Wall
 
 # Loop over all inputs starting with x_*.bin
-for xfile in inputs/x_*.bin
+for xfile in inputs_bin/x_*.bin
 do
     echo "Running for input file: $xfile"
     ./hadamard_exec "$xfile"
